46911683531a2836ffdbd20638d2371edbf5d42d
omoyenipaul <136048778+omoyenipaul@users.noreply.github.com>
2026-05-21 17:25:48 +0100

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 const Hero = () => {
   return (
     <section className="relative overflow-hidden bg-[#070c23] text-white">
-      <div className="mx-auto flex w-full md:max-w-6xl flex-col gap-12 px-6 pb-64 pt-5 md:pb-80 md:pt-16 items-center text-center">
+      <div className="mx-auto flex w-full md:max-w-6xl flex-col gap-12 px-6 pb-80 pt-5 md:pb-96 md:pt-16 items-center text-center">
         <div className="flex flex-col gap-6 md:max-w-5xl max-w-4xl">
           <h1 className="font-display text-[36px] font-semibold leading-tight !text-white">
             Ghostwriting for builders and creators who want to stay consistent
@@ -25,21 +25,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 w-full -mb-px h-[180px] sm:h-[220px] md:h-[280px]">
-        <svg
-          className="block h-full w-full"
-          viewBox="0 0 1440 120"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M0 60C360 100 1080 100 1440 60V120H0V60Z"
-            fill="#ffffff"
-          />
-        </svg>
-      </div>
+   <div className="absolute bottom-0 left-0 w-full h-[3px] bg-white"></div>
     </section>
   );
 };
